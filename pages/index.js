fc44f08e00e141a89c618375a97c6b650bbc74a8
Ramon van Raaij <ramon.van.raaij@gmail.com>
2022-08-13 13:59:11 +0200

    added next.js, Kendo-react and tailwindCSS

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button } from "@progress/kendo-react-buttons";
+import kendoka from "../assets/kendoka.svg";
+import Image from "next/image";
+
+export default function Home() {
+  const handleClick = React.useCallback(() => {
+    window.open("https://www.telerik.com/kendo-react-ui/components/", "_blank");
+  }, []);
+
+  return (
+    <div className="container p-5">
+      <header className="flex flex-col justify-content-center">
+        <Image src={kendoka} className="App-logo" alt="kendoka" />
+        <h1 className="text-3xl font-bold underline pb-5">
+          Edit <code>src/App.tsx</code> and save to reload.
+        </h1>
+        <Button themeColor={"primary"} size={"large"} onClick={handleClick}>
+          Learn KendoReact
+        </Button>
+      </header>
+    </div>
+  );
+}
